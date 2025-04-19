1f62fa9555e2dbbd9bde1fa60ea430a91b74b3d6
komorebi <markovs.i.mail@gmail.com>
2025-04-19 07:35:35 +0400

    Add /opt directory

--- build_files/scripts/enable-google-chrome.sh
+++ build_files/scripts/enable-google-chrome.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-set -oue pipefail 
+set -oue pipefail
+
+# Create /opt directory
+mkdir /var/opt
+mkdir /opt
+sudo ln -sfn /var/opt /opt
+
 # Part of an attempt to add Google Chrome in the usual way.
 echo "Fixing google-chrome yum repo"
 sed -i '/enabled/d' /etc/yum.repos.d/google-chrome.repo 
