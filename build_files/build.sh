40ff2b7ac3e31372411deea82d00aaf7825640f1
komorebi <markovs.i.mail@gmail.com>
2025-04-19 22:16:36 +0400

    Call install flatpaks

--- build_files/build.sh
+++ build_files/build.sh
@@ -5,6 +5,7 @@ set -ouex pipefail
 /ctx/scripts/cleanup.sh
 /ctx/scripts/install-google-chrome.sh
 /ctx/scripts/install-rpm-packages.sh
+/ctx/scripts/install-flatpaks.sh
 /ctx/scripts/user-settings.sh
 
 
