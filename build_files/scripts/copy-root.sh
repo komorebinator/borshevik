88f94cee996af7c9162df052cf104a276a4a3f7a
komorebi <markovs.i.mail@gmail.com>
2025-04-19 23:31:13 +0400

    Fix /usr/local somehow is not a directory

--- build_files/scripts/copy-root.sh
+++ build_files/scripts/copy-root.sh
@@ -1,8 +1,15 @@
-# Get the absolute path to the current script
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
+#!/bin/bash
+set -oue pipefail
 
-# Root folder is one level up from script directory
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 ROOT_DIR="${SCRIPT_DIR}/../root"
 
-# Copy everything (including hidden files and empty dirs)
+# Fix invalid /usr/local if needed
+if [ -e /usr/local ] && [ ! -d /usr/local ]; then
+    echo "Fixing /usr/local (was not a directory)"
+    rm -f /usr/local
+    mkdir -p /usr/local
+fi
+
+# Copy everything
 cp -r "${ROOT_DIR}/." /
