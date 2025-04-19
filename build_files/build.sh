17ef33ce56400b6bcb3af8fd53fa0262975b38b3
komorebi <markovs.i.mail@gmail.com>
2025-04-19 06:45:38 +0400

    Add google chrome, remove tmux

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,6 +2,10 @@
 
 set -ouex pipefail
 
+### Enable Google Chrome repo
+scripts/enable-google-chrome.sh
+
+
 ### Install packages
 
 # Packages can be installed from any enabled yum repo on the image.
@@ -10,7 +14,8 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 install -y google-chrome-stable
+
 
 # Use a COPR Example:
 #
