3d3c5f0ccb28f35f0df71888202f1dfa71fd3e5a
komorebi <markovs.i.mail@gmail.com>
2025-05-02 22:05:47 +0400

    Also remove classic session

--- build_files/scripts/cleanup.sh
+++ build_files/scripts/cleanup.sh
@@ -2,4 +2,4 @@
 
 set -ouex pipefail
 
-rpm-ostree override remove firefox firefox-langpacks toolbox gnome-shell-extension-*
+rpm-ostree override remove firefox firefox-langpacks toolbox gnome-classic-session gnome-shell-extension-*
