c9c7c06699a9f88cccfe5eefc6801c027a6ccc34
komorebi <markovs.i.mail@gmail.com>
2025-05-06 20:49:00 +0400

    Fix AI mistake using human brain

--- build_files/scripts/install-gs-extensions.sh
+++ build_files/scripts/install-gs-extensions.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 DEST="/usr/share/gnome-shell/extensions"
 TMP_DIR="$(mktemp -d)"
 
-git clone --depth 1 https://github.com/Tudmotu/gnome-shell-extension-clipboard-indicator.git "$TMP_DIR"
+git clone --depth 1 https://github.com/Tudmotu/gnome-shell-extension-clipboard-indicator.git "$TMP_DIR/clipboard-indicator@tudmotu.com"
 rsync -a --exclude=.git "$TMP_DIR/" "$DEST/"
 rm -rf "$TMP_DIR"
