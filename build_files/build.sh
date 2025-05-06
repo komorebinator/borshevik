25c50f61202ce81e9160b597e4621f98525dc44a
komorebi <markovs.i.mail@gmail.com>
2025-05-06 20:33:50 +0400

    Move extensions installer up

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,10 +4,10 @@ set -ouex pipefail
 
 /ctx/scripts/cleanup.sh
 /ctx/scripts/copy-root.sh
+/ctx/scripts/install-gs-extensions.sh
 /ctx/scripts/install-copr-packages.sh
 /ctx/scripts/install-google-chrome.sh
 /ctx/scripts/install-rpm-packages.sh
-/ctx/scripts/install-gs-extensions.sh
 /ctx/scripts/user-settings.sh
 
 systemctl enable podman.socket
