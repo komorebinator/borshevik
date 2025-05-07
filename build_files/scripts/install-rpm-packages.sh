6aee06a108ebe51c2a72b3765410584122585a79
komorebi <markovs.i.mail@gmail.com>
2025-05-07 21:48:23 +0400

    Take gsconnect + nautilus extensions from package

--- build_files/scripts/install-rpm-packages.sh
+++ build_files/scripts/install-rpm-packages.sh
@@ -2,4 +2,4 @@
 
 set -ouex pipefail
 
-rpm-ostree install -y htop mc gnome-tweaks pwgen openssl distrobox gnome-disk-utility zsh
+rpm-ostree install -y htop mc gnome-tweaks pwgen openssl distrobox gnome-disk-utility zsh gnome-shell-extension-gsconnect nautilus-gsconnect python3-nautilus
