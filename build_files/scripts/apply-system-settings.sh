8219b16cceb43000cc4623146ddfe740e492d742
komorebi <markovs.i.mail@gmail.com>
2025-05-12 00:19:46 +0400

    Enable all extensions by default

--- build_files/scripts/apply-system-settings.sh
+++ build_files/scripts/apply-system-settings.sh
@@ -2,13 +2,30 @@
 set -oue pipefail
 
 # Create override file to set default GNOME favorites (dock icons)
-cat <<EOF > /usr/share/glib-2.0/schemas/99-chrome-favorite.gschema.override
+cat <<EOF >/usr/share/glib-2.0/schemas/99-chrome-favorite.gschema.override
 [org.gnome.shell]
 favorite-apps=['google-chrome.desktop', 'org.gnome.Nautilus.desktop']
 EOF
 
 # Compile schemas so GNOME picks up the new defaults
 glib-compile-schemas /usr/share/glib-2.0/schemas
+
+# Lookup for extensions to enabled
+EXT_DIRS=(/usr/share/gnome-shell/extensions/*)
+LIST=""
+for d in "${EXT_DIRS[@]}"; do
+    NAME=$(basename "$d")
+    LIST="$LIST,'$NAME'"
+done
+LIST="[${LIST#,}]"
+
+# Enable extensions by default
+mkdir -p /etc/dconf/db/local.d
+cat <<EOF >/etc/dconf/db/local.d/00-extensions
+[org/gnome/shell]
+enabled-extensions=$LIST
+EOF
+dconf update
 
 systemctl enable app-choice-subscription.service
 systemctl enable setup-kargs.service
