77c9e4503698809407428455f77cf7fcada36da2
komorebi <markovs.i.mail@gmail.com>
2025-05-12 21:51:45 +0400

    Trying out another way to apply dconf settings

--- build_files/scripts/apply-dconf.sh
+++ build_files/scripts/apply-dconf.sh
@@ -3,6 +3,7 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 dconf compile /usr/share/gdm/greeter-dconf-defaults "$SCRIPT_DIR/dconf/gdm.d"
+dconf compile /etc/dconf/db/local "$SCRIPT_DIR/dconf/local.d"
 
 echo "→ Enable extensions by default"
 EXT_DIRS=(/usr/share/gnome-shell/extensions/*)
@@ -13,15 +14,9 @@ for d in "${EXT_DIRS[@]}"; do
 done
 LIST="[${LIST#,}]"
 
-mkdir -p /etc/dconf/db/local.d
 cat <<EOF >/etc/dconf/db/local.d/00-extensions
 [org/gnome/shell]
 enabled-extensions=$LIST
 EOF
-
-echo "→ Installing additional user-default INI files"
-for ini in "$SCRIPT_DIR"/dconf/local.d/*; do
-    cp "$ini" /etc/dconf/db/local.d/$(basename "$ini")
-done
 
 dconf update
