8934de287628b71c2e9b5d680ab8257105284d12
komorebi <markovs.i.mail@gmail.com>
2025-05-17 15:39:56 +0400

    Add patch utility to the container

--- build_files/scripts/build.sh
+++ build_files/scripts/build.sh
@@ -3,10 +3,10 @@
 set -ouex pipefail
 
 /build_scripts/cleanup.sh
+/build_scripts/install-rpm-packages.sh
 /build_scripts/install-gs-extensions.sh
 /build_scripts/install-copr-packages.sh
 /build_scripts/install-google-chrome.sh
-/build_scripts/install-rpm-packages.sh
 /build_scripts/apply-schemas.sh
 /build_scripts/apply-dconf.sh
 /build_scripts/enable-services.sh
--- build_files/scripts/install-rpm-packages.sh
+++ build_files/scripts/install-rpm-packages.sh
@@ -2,4 +2,4 @@
 
 set -ouex pipefail
 
-rpm-ostree install -y htop mc gnome-tweaks pwgen openssl distrobox gnome-disk-utility zsh gnome-shell-extension-gsconnect nautilus-gsconnect nautilus-python
+rpm-ostree install -y htop mc gnome-tweaks pwgen openssl distrobox gnome-disk-utility zsh gnome-shell-extension-gsconnect nautilus-gsconnect nautilus-python patch
