7d7f71e8512b47aea789867a251f035974165625
komorebi <markovs.i.mail@gmail.com>
2025-06-21 18:37:52 +0400

    Fixes for the previous commit

--- build_files/scripts/os-info.sh
+++ build_files/scripts/os-info.sh
@@ -6,4 +6,5 @@ if [ -n "${IMAGE_TAG:-}" ]; then
     sed -i "s|^VERSION=.*|VERSION=\"${IMAGE_TAG}\"|" /etc/os-release
     sed -i "s|^PRETTY_NAME=.*|PRETTY_NAME=\"Borshevik (${IMAGE_TAG})\"|" /etc/os-release
     sed -i "s|^VERSION_ID=.*|VERSION_ID=\"${FEDORA_MAJOR_VERSION}\"|" /etc/os-release
+    sed -i "s|^VARIANT_ID=.*|VARIANT_ID=\"${IMAGE_NAME}\"|" /etc/os-release
 fi
