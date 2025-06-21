d419aa5b4a28a12435838c02500e302f90a32628
komorebi <markovs.i.mail@gmail.com>
2025-06-21 21:17:49 +0400

    Remove /etc/os-release and make use /usr/lib/os-release

--- build_files/scripts/os-info.sh
+++ build_files/scripts/os-info.sh
@@ -3,8 +3,8 @@
 set -euo pipefail
 
 if [ -n "${IMAGE_TAG:-}" ]; then
-    sed -i "s|^VERSION=.*|VERSION=\"${IMAGE_TAG}\"|" /etc/os-release
-    sed -i "s|^PRETTY_NAME=.*|PRETTY_NAME=\"Borshevik (${IMAGE_TAG})\"|" /etc/os-release
-    sed -i "s|^VERSION_ID=.*|VERSION_ID=\"${FEDORA_MAJOR_VERSION}\"|" /etc/os-release
-    sed -i "s|^VARIANT_ID=.*|VARIANT_ID=\"${IMAGE_NAME}\"|" /etc/os-release
+    sed -i "s|^VERSION=.*|VERSION=\"${IMAGE_TAG}\"|" /usr/lib/os-release
+    sed -i "s|^PRETTY_NAME=.*|PRETTY_NAME=\"Borshevik (${IMAGE_TAG})\"|" /usr/lib/os-release
+    sed -i "s|^VERSION_ID=.*|VERSION_ID=\"${FEDORA_MAJOR_VERSION}\"|" /usr/lib/os-release
+    sed -i "s|^VARIANT_ID=.*|VARIANT_ID=\"${IMAGE_NAME}\"|" /usr/lib/os-release
 fi
