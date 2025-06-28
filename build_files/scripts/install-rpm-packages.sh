53ff65f5768dd0115f8d8a782d55c785dec0c2b8
komorebi <markovs.i.mail@gmail.com>
2025-06-28 16:34:26 +0400

    Adding glib2-devel required for color picker

--- build_files/scripts/install-rpm-packages.sh
+++ build_files/scripts/install-rpm-packages.sh
@@ -2,4 +2,4 @@
 
 set -ouex pipefail
 
-rpm-ostree install -y htop mc gnome-tweaks pwgen openssl distrobox gnome-disk-utility zsh gnome-shell-extension-gsconnect nautilus-python patch gnome-themes-extra meson ninja-build
+rpm-ostree install -y htop mc gnome-tweaks pwgen openssl distrobox gnome-disk-utility zsh gnome-shell-extension-gsconnect nautilus-python patch gnome-themes-extra meson ninja-build glib2-devel
