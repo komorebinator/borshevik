13d010ac64a9f1f7d1962932d1a9ba118a187bd1
komorebi <markovs.i.mail@gmail.com>
2025-07-05 01:16:48 +0400

    Revert to ptyxis because ghostty is unstable

--- build_files/scripts/cleanup.sh
+++ build_files/scripts/cleanup.sh
@@ -2,6 +2,6 @@
 
 set -ouex pipefail
 
-rpm-ostree override remove firefox firefox-langpacks toolbox gnome-classic-session gnome-shell-extension-apps-menu gnome-shell-extension-launch-new-instance gnome-shell-extension-places-menu gnome-shell-extension-window-list gnome-shell-extension-background-logo nvtop ptyxis
+rpm-ostree override remove firefox firefox-langpacks toolbox gnome-classic-session gnome-shell-extension-apps-menu gnome-shell-extension-launch-new-instance gnome-shell-extension-places-menu gnome-shell-extension-window-list gnome-shell-extension-background-logo nvtop
 
 systemctl disable flatpak-add-fedora-repos.service
--- build_files/scripts/install-copr-packages.sh
+++ build_files/scripts/install-copr-packages.sh
@@ -7,7 +7,3 @@ if [[ "$(rpm -q --qf '%{EPOCH}' gnome-control-center)" != "1" ]]; then
     echo "Gnome control center was not upgraded" >&2
     exit 1
 fi
-
-dnf5 -y copr enable pgdev/ghostty
-
-dnf install -y ghostty
