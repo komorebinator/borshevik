d58b9ff57cfe799b28dc760b3bca1d3543a51047
komorebi <markovs.i.mail@gmail.com>
2025-07-04 19:14:40 +0400

    Delete fedora from fralpak remotes

--- build_files/scripts/cleanup.sh
+++ build_files/scripts/cleanup.sh
@@ -3,3 +3,5 @@
 set -ouex pipefail
 
 rpm-ostree override remove firefox firefox-langpacks toolbox gnome-classic-session gnome-shell-extension-apps-menu gnome-shell-extension-launch-new-instance gnome-shell-extension-places-menu gnome-shell-extension-window-list gnome-shell-extension-background-logo nvtop ptyxis
+
+flatpak remote-delete fedora
