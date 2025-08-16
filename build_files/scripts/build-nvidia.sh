b6900cd270e70476af420e961d77002a50896790
komorebi <markovs.i.mail@gmail.com>
2025-08-16 14:35:02 +0400

    Trying out negativo17 nvidia driver

--- build_files/scripts/build-nvidia.sh
+++ build_files/scripts/build-nvidia.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 set -oue pipefail
 
-KVER="$(rpm -q --qf '%{VERSION}-%{RELEASE}.%{ARCH}\n' kernel-core)"
+#KVER="$(rpm -q --qf '%{VERSION}-%{RELEASE}.%{ARCH}\n' kernel-core)"
 
 # Add RPM Fusion repositories
-dnf5 install -y \
-  https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
-  https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+#dnf5 install -y \
+  #https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+  #https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 # Install NVIDIA driver packages
-rpm-ostree install -y akmod-nvidia xorg-x11-drv-nvidia xorg-x11-drv-nvidia-cuda nvidia-settings kernel-devel kernel-headers
-akmods --force --kernels "$KVER"
+#rpm-ostree install -y akmod-nvidia xorg-x11-drv-nvidia xorg-x11-drv-nvidia-cuda nvidia-settings kernel-devel kernel-headers
+#akmods --force --kernels "$KVER"
+
+curl -o /etc/yum.repos.d/fedora-nvidia.repo https://negativo17.org/repos/fedora-nvidia.repo
+
+rpm-ostree install nvidia-driver akmod-nvidia nvidia-settings
