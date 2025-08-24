7ddaae3bfaeedd140d5001619669002a16cbdd67
komorebi <markovs.i.mail@gmail.com>
2025-08-24 17:03:13 +0400

    Trying to pull steamdirectly from negativo17

--- build_files/scripts/install-copr-packages.sh
+++ build_files/scripts/install-copr-packages.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 dnf5 -y copr enable komorebithrows/borshevik
 
-rpm-ostree override remove gnome-control-center
-rpm-ostree install gnome-control-center
+dnf upgrade -y gnome-control-center
 
 if [[ "$(rpm -q --qf '%{EPOCH}' gnome-control-center)" != "1" ]]; then
     echo "Gnome control center was not upgraded" >&2
     exit 1
 fi
 
-dnf -y install \
-  https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
-  https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-
-rpm-ostree install -y libva.i686 mesa-dri-drivers.i686 mesa-vulkan-drivers.i686 vulkan-loader.i686
-rpm-ostree install -y steam steam-devices
+dnf config-manager addrepo --from-repofile=https://negativo17.org/repos/fedora-steam.repo
+rpm-ostree install -y steam kernel-modules-extra
