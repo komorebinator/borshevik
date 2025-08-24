00818c992af5e064416bd151e089a54eb63e3c3f
komorebi <markovs.i.mail@gmail.com>
2025-08-24 16:19:45 +0400

    Install local steam

--- build_files/scripts/install-copr-packages.sh
+++ build_files/scripts/install-copr-packages.sh
@@ -7,3 +7,9 @@ if [[ "$(rpm -q --qf '%{EPOCH}' gnome-control-center)" != "1" ]]; then
     echo "Gnome control center was not upgraded" >&2
     exit 1
 fi
+
+dnf -y install \
+  https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+  https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+rpm-ostree install -y steam steam-devices
