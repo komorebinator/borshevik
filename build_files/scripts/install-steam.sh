d5aa93f39becc0844a546082fba7eab54a059108
komorebi <markovs.i.mail@gmail.com>
2025-09-20 20:22:55 +0400

    Try to disable fedora repos during steam installation

--- build_files/scripts/install-steam.sh
+++ build_files/scripts/install-steam.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 set -ouex pipefail
 
+sed -i 's/^\s*enabled\s*=.*/enabled=0/' \
+  /etc/yum.repos.d/fedora.repo \
+  /etc/yum.repos.d/fedora-updates.repo \
+  /etc/yum.repos.d/fedora-updates-archive.repo
+
 rpm-ostree -y install \
   https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
   https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
-  rpm-ostree -y install steam steam-devices
+rpm-ostree -y install steam steam-devices
+
+sed -i 's/^\s*enabled\s*=.*/enabled=1/' \
+  /etc/yum.repos.d/fedora.repo \
+  /etc/yum.repos.d/fedora-updates.repo \
+  /etc/yum.repos.d/fedora-updates-archive.repo
