c264ddd2f63deac89cd66a9c7c8ab196b878e415
komorebi <markovs.i.mail@gmail.com>
2025-09-20 18:05:13 +0400

    Install repos instead of trying to enable them

--- build_files/scripts/build-nvidia.sh
+++ build_files/scripts/build-nvidia.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# ЖЁСТКИЙ путь к nvidia-vars из слоёв akmods:
-# (у тебя kmods и ublue-os лежат под /tmp/akmods-nvidia/rpms/)
 NVVARS="/tmp/akmods-nvidia/rpms/kmods/nvidia-vars"
-source "$NVVARS"  # даёт NVIDIA_AKMOD_VERSION (и др.)
+source "$NVVARS"
 ver="${NVIDIA_AKMOD_VERSION}"
+echo "Nvidia akmod version ${ver}"
 
 # enable negativo and container toolkit repo
-sed -i 's/^\s*enabled\s*=\s*0/enabled=1/' /etc/yum.repos.d/negativo17-fedora-nvidia.repo
-sed -i 's/^\s*enabled\s*=\s*0/enabled=1/' /etc/yum.repos.d/nvidia-container-toolkit.repo
+#sed -i 's/^\s*enabled\s*=\s*0/enabled=1/' /etc/yum.repos.d/negativo17-fedora-nvidia.repo
+#sed -i 's/^\s*enabled\s*=\s*0/enabled=1/' /etc/yum.repos.d/nvidia-container-toolkit.repo
 
-#curl -fsSL --retry 5 -o /etc/yum.repos.d/negativo17-fedora-nvidia.repo https://negativo17.org/repos/fedora-nvidia.repo
-#curl -fsSL --retry 5 -o /etc/yum.repos.d/nvidia-container-toolkit.repo https://nvidia.github.io/libnvidia-container/stable/rpm/nvidia-container-toolkit.repo
+curl -fsSL --retry 5 -o /etc/yum.repos.d/negativo17-fedora-nvidia.repo https://negativo17.org/repos/fedora-nvidia.repo
+curl -fsSL --retry 5 -o /etc/yum.repos.d/nvidia-container-toolkit.repo https://nvidia.github.io/libnvidia-container/stable/rpm/nvidia-container-toolkit.repo
 
 # Install drivers
 dnf -y install \
@@ -23,11 +22,8 @@ dnf -y install \
   nvidia-container-toolkit \
   libva-nvidia-driver
 
-# Отключаем репы обратно
-sed -i 's/enabled=1/enabled=0/g' /etc/yum.repos.d/negativo17-fedora-nvidia.repo || true
-sed -i 's/enabled=1/enabled=0/g' /etc/yum.repos.d/nvidia-container-toolkit.repo || true
 
-# Санити-проверка версии
+# Check
 inst_ver="$(rpm -q --qf '%{VERSION}\n' nvidia-driver | head -n1 || true)"
 [[ "$inst_ver" == "$ver" ]] || { echo "nvidia-driver ${inst_ver} != ${ver}"; exit 3; }
 
