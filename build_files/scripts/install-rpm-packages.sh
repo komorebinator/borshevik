47cf0155556de6a0039f1b2330852f738ec59760
komorebi <markovs.i.mail@gmail.com>
2025-10-08 03:21:32 +0400

    Add QT devel dependencies

--- build_files/scripts/install-rpm-packages.sh
+++ build_files/scripts/install-rpm-packages.sh
@@ -2,4 +2,4 @@
 
 set -ouex pipefail
 rpm-ostree --experimental override replace --from repo=fedora --from repo=updates gnome-software
-rpm-ostree install -y htop mc gnome-tweaks pwgen openssl distrobox gnome-disk-utility zsh gnome-shell-extension-gsconnect nautilus-python patch gnome-themes-extra meson ninja-build glib2-devel sassc translate-shell util-linux gnome-software-rpm-ostree PackageKit
+rpm-ostree install -y htop mc gnome-tweaks pwgen openssl distrobox gnome-disk-utility zsh gnome-shell-extension-gsconnect nautilus-python patch gnome-themes-extra meson ninja-build glib2-devel sassc translate-shell util-linux gnome-software-rpm-ostree PackageKit xcb-util-cursor xcb-util-cursor-devel
