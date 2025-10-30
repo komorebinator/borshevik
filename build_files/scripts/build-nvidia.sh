94e96177918e5d4894b727dec0c27adc24a989ac
komorebi <markovs.i.mail@gmail.com>
2025-10-30 13:03:16 +0400

    Trying to fix nvidia

--- build_files/scripts/build-nvidia.sh
+++ build_files/scripts/build-nvidia.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+rpm-ostree override remove kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra
+
 NVVARS="/tmp/akmods-nvidia/rpms/kmods/nvidia-vars"
 source "$NVVARS"
 ver="${NVIDIA_AKMOD_VERSION}"
 echo "Nvidia akmod version ${ver}"
-
-#curl -fsSL --retry 5 -o /etc/yum.repos.d/negativo17-fedora-nvidia.repo https://negativo17.org/repos/fedora-nvidia.repo
-#curl -fsSL --retry 5 -o /etc/yum.repos.d/nvidia-container-toolkit.repo https://nvidia.github.io/libnvidia-container/stable/rpm/nvidia-container-toolkit.repo
 
 # Install drivers
 rpm-ostree -y install /tmp/akmods-nvidia/rpms/ublue-os/ublue-os-nvidia*.rpm
