420c44b417cdd64a2b9b423b5d8f4629efae6cf4
komorebi <markovs.i.mail@gmail.com>
2025-11-04 01:58:05 +0400

    Fix gsconnect install script

--- build_files/scripts/gs-extensions/gsconnect.sh
+++ build_files/scripts/gs-extensions/gsconnect.sh
@@ -9,5 +9,5 @@ meson setup _build . \
   -Dsession_bus_services_dir=/usr/share/dbus-1/services \
   -Dwebextension=false \
   -Dnautilus=true
-meson compile -C .
-meson install  -C .
+meson compile -C _build
+meson install -C _build
