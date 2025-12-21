d59e2b48f2ce0bf7379217a080063c754bfc18ed
komorebi <markovs.i.mail@gmail.com>
2025-12-21 06:40:24 +0400

    Try to install steam

---
+++ build_files/scripts/install-steam.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -ouex pipefail
+
+rpm-ostree -y install \
+  https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+  https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+rpm-ostree -y install steam steam-devices
