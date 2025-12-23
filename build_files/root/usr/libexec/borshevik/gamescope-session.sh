7b2007219f721d03c8d4d83abd84ab47a5ba5fd8
komorebi <markovs.i.mail@gmail.com>
2025-12-23 04:01:59 +0400

    Fix layer name

--- build_files/root/usr/libexec/borshevik/gamescope-session.sh
+++ build_files/root/usr/libexec/borshevik/gamescope-session.sh
@@ -2,7 +2,7 @@
 
 STEAM_CMD=(steam -gamepadui)
 
-export VK_LOADER_LAYERS_DISABLE=VK_LAYER_FROG_gamescope_wsi
+export VK_LOADER_LAYERS_DISABLE=VK_LAYER_FROG_gamescope_wsi_x86_64
 
 exec gamescope -e --expose-wayland --adaptive-sync -- "${STEAM_CMD[@]}"
 
