bb7967a892f487f774476b1ab892392f9e0f36b5
komorebi <markovs.i.mail@gmail.com>
2025-12-24 01:58:22 +0400

    A different way to remove all extensions

--- build_files/scripts/cleanup.sh
+++ build_files/scripts/cleanup.sh
@@ -2,6 +2,17 @@
 
 set -ouex pipefail
 
-rpm-ostree override remove firefox firefox-langpacks toolbox gnome-classic-session gnome-shell-extension-apps-menu gnome-shell-extension-launch-new-instance gnome-shell-extension-places-menu gnome-shell-extension-window-list gnome-shell-extension-background-logo nvtop
+rpm-ostree override remove firefox firefox-langpacks toolbox gnome-classic-session nvtop
+
+echo "Removing Gnome Shell extension rpms";
+mapfile -t EXT_PKGS < <(rpm -qa --qf '%{NAME}\n' 'gnome-shell-extension-*' | sort -u)
+
+if ((${#EXT_PKGS[@]})); then
+  echo "Removing GNOME Shell extension RPMs:"
+  printf '  %s\n' "${EXT_PKGS[@]}"
+  rpm-ostree override remove "${EXT_PKGS[@]}"
+else
+  echo "No gnome-shell-extension-* RPMs installed, skipping."
+fi
 
 systemctl disable flatpak-add-fedora-repos.service
