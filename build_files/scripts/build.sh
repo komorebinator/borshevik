cf332b3b7ca400e808259c8e3d4e239a4f0fa110
komorebi <markovs.i.mail@gmail.com>
2025-12-29 01:16:33 +0400

    Cleanup after all things are installed

--- build_files/scripts/build.sh
+++ build_files/scripts/build.sh
@@ -3,11 +3,11 @@
 set -ouex pipefail
 
 /build_scripts/os-info.sh
-/build_scripts/cleanup.sh
 /build_scripts/install-rpm-packages.sh
-/build_scripts/install-gs-extensions.sh
 /build_scripts/install-google-chrome.sh
 /build_scripts/install-steam.sh
+/build_scripts/cleanup.sh
+/build_scripts/install-gs-extensions.sh
 /build_scripts/apply-schemas.sh
 /build_scripts/apply-dconf.sh
 /build_scripts/enable-services.sh
