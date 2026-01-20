a1493a3ec1cbcfdefefc8b9c7eddb5f5d7350f7e
komorebi <markovs.i.mail@gmail.com>
2026-01-20 08:30:59 +0400

    Fix for the previous commit

--- build_files/scripts/install-rpm-packages.sh
+++ build_files/scripts/install-rpm-packages.sh
@@ -3,4 +3,3 @@
 set -ouex pipefail
 #rpm-ostree --experimental override replace --from repo=fedora --from repo=updates gnome-software
 rpm-ostree install -y htop mc gnome-tweaks pwgen openssl distrobox gnome-disk-utility zsh gnome-shell-extension-gsconnect nautilus-python patch gnome-themes-extra meson ninja-build gcc pkgconf-pkg-config make cmake glib2-devel sassc translate-shell util-linux gnome-software-rpm-ostree PackageKit xcb-util-cursor xcb-util-cursor-devel gh
-цвета
