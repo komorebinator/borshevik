4afd962135cf6e5246f2ed7af493c7f2551b5416
komorebi <markovs.i.mail@gmail.com>
2026-01-21 14:05:32 +0400

    Add libxcrypt-compat

--- build_files/scripts/install-rpm-packages.sh
+++ build_files/scripts/install-rpm-packages.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 set -ouex pipefail
-rpm-ostree install -y htop mc gnome-tweaks pwgen openssl distrobox gnome-disk-utility zsh gnome-shell-extension-gsconnect nautilus-python patch gnome-themes-extra meson ninja-build gcc pkgconf-pkg-config make cmake glib2-devel sassc translate-shell util-linux xcb-util-cursor xcb-util-cursor-devel gh
+rpm-ostree install -y htop mc gnome-tweaks pwgen openssl distrobox gnome-disk-utility zsh gnome-shell-extension-gsconnect nautilus-python patch gnome-themes-extra meson ninja-build gcc pkgconf-pkg-config make cmake glib2-devel sassc translate-shell util-linux xcb-util-cursor xcb-util-cursor-devel gh libxcrypt-compat
