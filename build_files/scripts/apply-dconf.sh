f76102e5acbfa390681dc9f158c59fc4e433a9c5
komorebi <markovs.i.mail@gmail.com>
2026-05-07 16:51:32 +0400

    Add more log information

--- build_files/scripts/apply-dconf.sh
+++ build_files/scripts/apply-dconf.sh
@@ -2,12 +2,14 @@
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+echo "→ Apply gdm.d dconf"
 mkdir -p /etc/dconf/db/gdm.d
 cp "$SCRIPT_DIR/dconf/gdm.d/gdm" /etc/dconf/db/gdm.d/00-borshevik
 
 mkdir -p /etc/dconf/profile
 printf 'user-db:user\nsystem-db:gdm\nfile-db:/usr/share/gdm/greeter-dconf-defaults\n' > /etc/dconf/profile/gdm
 
+echo "→ Apply local.d dconf"
 cp -a "$SCRIPT_DIR/dconf/local.d/." /etc/dconf/db/local.d/
 
 echo "→ Enable extensions by default"
